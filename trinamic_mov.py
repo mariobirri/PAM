f80b333a3c0404d5c541e2cd845d0c0b0e459285
mariobirri <mario.birri@psi.ch>
2020-10-26 19:54:14 +0000

    geändert:       trinamic_mov.py 	geändert:       variables.py

--- trinamic_mov.py
+++ trinamic_mov.py
@@ -12,49 +12,44 @@ import time
 connectionManager = ConnectionManager()
 myInterface = connectionManager.connect()
 PyTrinamic.showInfo()
-M1 = MOT(myInterface,0)
-M2 = MOT(myInterface,1)
-M3 = MOT(myInterface,2)
+M0 = MOT(myInterface,0)
+M1 = MOT(myInterface,1)
+
+M1.setAxisParameter(140, 5) #microstep 32
+M1.setAxisParameter(6, 24) # run current
+M1.setAxisParameter(7, 8) # halt current
+M1.setAxisParameter(174, -2)
+M1.setAxisParameter(182, 14000)
+M1.setAxisParameter(181, 14000)
+M1.setAxisParameter(5, 400000)
+M1.setMaxVelocity(15000)
+
+topic =['STOP','M1.HOME','M1.SPEED','M1.POS','M1.RBV','M1.SCAN']
 
 
-print("-----------------")
-#print(driverBoard.showMotionConfiguration())
-print("-----------------")
-
-M2.setAxisParameter(140, 5) #microstep 32
-M2.setAxisParameter(6, 24) # run current
-M2.setAxisParameter(7, 8) # halt current
-M2.setAxisParameter(174, -2)
-M2.setAxisParameter(182, 14000)
-
-M2.setAxisParameter(181, 14000)
-M2.setAxisParameter(5, 400000)
+# convert a msg to an int value
+def msg2Int(inMsg):
+	return int(inMsg.payload.decode('UTF-8'))
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
 	print("Connected with result code "+str(rc))
-	client.subscribe(var.topic_mot1_pos)
-	client.subscribe('STOP')
-	client.subscribe(var.topic_mot1_rbv)
-	client.subscribe(var.topic_mot1_rbc)
-	client.subscribe('M1.HOME')
+	for x in topic:
+		client.subscribe(x)
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
 
-	if msg.topic == var.topic_mot1_pos:
-		pos = int(msg.payload.decode('UTF-8'))
-		M2.moveTo(pos, 15000)
-
+	if msg.topic == 'STOP': M1.stop()
+	elif msg.topic == 'M1.SCAN': publish.single('M1.RBV', M1.getActualPosition(), hostname=var.mqtt_server)
+	elif msg.topic == 'M1.POS': M1.moveTo(msg2Int(msg), M1.getMaxVelocity())
+	elif msg.topic == 'M1.SPEED': M1.setMaxVelocity(msg2Int(msg))
 	elif msg.topic == 'M1.HOME':
-		M2.rotate(15000)
-		while M2.getAxisParameter(3) != 0: time.sleep(0.1)
-		M2.stop()
-		M2.setAxisParameter(1, 0)
+		M1.rotate(15000)
+		while M1.getAxisParameter(3) != 0: time.sleep(0.1)
+		M1.stop()
+		M1.setAxisParameter(1, 0)
 
-	elif msg.topic == var.topic_mot1_rbc: rbv = M2.getActualPosition(); publish.single(var.topic_mot1_rbv, rbv, hostname=var.mqtt_server)
-	elif msg.topic == 'STOP':
-		M2.stop()
 
 client = mqtt.Client()
 client.on_connect = on_connect
