4940a57ba87480d5f6705e260e94e6a1aa10a25f
mariobirri <mario.birri@psi.ch>
2020-10-30 19:32:39 +0100

    geändert:       epicsDriver.py 	gelöscht:       pam.py.save 	gelöscht:       pam.py.save.1 	gelöscht:       pam.py.save.2

--- epicsDriver.py
+++ epicsDriver.py
@@ -11,71 +11,13 @@ class myDriver(Driver):
 		super(myDriver, self).__init__()
 
 	def read(self, reason):
-		#print(subscribe.simple(reason, hostname=var.mqtt_server).payload);
+
 		return float(subscribe.simple(reason, hostname=var.mqtt_server).payload);
-		'''
-		if reason == 'M0.RBV': value = subscribe.simple('M0.RBV', hostname=var.mqtt_server).payload;
-                elif  reason == 'M1.RBV': value = subscribe.simple('M1.RBV', hostname=var.mqtt_server).payload;
-                elif  reason == 'M2.RBV': value = subscribe.simple('M2.RBV', hostname=var.mqtt_server).payload;
-                elif  reason == 'M3.RBV': value = subscribe.simple('M3.RBV', hostname=var.mqtt_server).payload;
-                elif  reason == 'M4.RBV': value = subscribe.simple('M4.RBV', hostname=var.mqtt_server).payload;
-                elif  reason == 'M5.RBV': value = subscribe.simple('M5.RBV', hostname=var.mqtt_server).payload;
-		
-		
-		else: value = self.getParam(reason)
-		return value
-		'''
-		
+
 	def write(self, reason, value):
         	status = True
-		#print(value)
-		#print(reason)
 		publish.single(reason, str(value), hostname=var.mqtt_server);
-		'''
-		if reason == 'STOP': publish.single('STOP', value, hostname=var.mqtt_server);
-			
-		elif reason == 'M0.HOME': 	publish.single('M0.HOME', value, hostname=var.mqtt_server);
-                elif reason == 'M0.SPEED': 	publish.single('M0.SPEED', value, hostname=var.mqtt_server);
-                elif reason == 'M0.POS': 	publish.single('M0.POS', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWR': 	publish.single('M0.TWR', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWF': 	publish.single('M0.TWF', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWV': 	publish.single('M0.TWV', value, hostname=var.mqtt_server);
-			
-		elif reason == 'M0.HOME': 	publish.single('M0.HOME', value, hostname=var.mqtt_server);
-                elif reason == 'M0.SPEED': 	publish.single('M0.SPEED', value, hostname=var.mqtt_server);
-                elif reason == 'M0.POS': 	publish.single('M0.POS', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWR': 	publish.single('M0.TWR', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWF': 	publish.single('M0.TWF', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWV': 	publish.single('M0.TWV', value, hostname=var.mqtt_server);                
 
-		elif reason == 'M0.HOME': 	publish.single('M0.HOME', value, hostname=var.mqtt_server);
-                elif reason == 'M0.SPEED': 	publish.single('M0.SPEED', value, hostname=var.mqtt_server);
-                elif reason == 'M0.POS': 	publish.single('M0.POS', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWR': 	publish.single('M0.TWR', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWF': 	publish.single('M0.TWF', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWV': 	publish.single('M0.TWV', value, hostname=var.mqtt_server);
-			
-		elif reason == 'M0.HOME': 	publish.single('M0.HOME', value, hostname=var.mqtt_server);
-                elif reason == 'M0.SPEED': 	publish.single('M0.SPEED', value, hostname=var.mqtt_server);
-                elif reason == 'M0.POS': 	publish.single('M0.POS', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWR': 	publish.single('M0.TWR', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWF': 	publish.single('M0.TWF', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWV': 	publish.single('M0.TWV', value, hostname=var.mqtt_server);
-			
-		elif reason == 'M0.HOME': 	publish.single('M0.HOME', value, hostname=var.mqtt_server);
-                elif reason == 'M0.SPEED': 	publish.single('M0.SPEED', value, hostname=var.mqtt_server);
-                elif reason == 'M0.POS': 	publish.single('M0.POS', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWR': 	publish.single('M0.TWR', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWF': 	publish.single('M0.TWF', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWV': 	publish.single('M0.TWV', value, hostname=var.mqtt_server);
-			
-		elif reason == 'M0.HOME': 	publish.single('M0.HOME', value, hostname=var.mqtt_server);
-                elif reason == 'M0.SPEED': 	publish.single('M0.SPEED', value, hostname=var.mqtt_server);
-                elif reason == 'M0.POS': 	publish.single('M0.POS', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWR': 	publish.single('M0.TWR', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWF': 	publish.single('M0.TWF', value, hostname=var.mqtt_server);
-                elif reason == 'M0.TWV': 	publish.single('M0.TWV', value, hostname=var.mqtt_server);
-		'''
         	if status:
 			self.setParam(reason, value)
 			return status
@@ -84,7 +26,7 @@ if __name__ == '__main__':
     server = SimpleServer()
     server.createPV(ed.prefix, ed.pvdb)
     driver = myDriver()
-    print("connected")
+    print("Connected Epics")
 
 while True:
 	server.process(0.2)
